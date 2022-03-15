7f723124b9d90c0f570ba6d1cf788eb7d6ba5ea4
Kadir Korkmaz <35937094+cskadir@users.noreply.github.com>
2022-03-15 07:02:31 +0300

    Update odev9.sql

--- sql/odev9.sql
+++ sql/odev9.sql
@@ -2,7 +2,7 @@ SELECT city, country FROM city
 JOIN country ON city.country_id =country.country_id;
 
 
-Select payment _id, first_name, last_name FROM customer
+Select payment_id, first_name, last_name FROM customer
 JOIN payment ON customer.customer_id = payment.customer_id
 
 SELECT rental.rental_id, customer.first_name, customer.last_name FROM customer
